55cbd7d93c7ecab9cf299bdf6857532861861761
qianxi5 <66296940+qianxi5@users.noreply.github.com>
2021-03-03 23:38:15 +0800

    Update UploadActivity.java

--- CabDab/app/src/main/java/com/example/cabdab/UploadActivity.java
+++ CabDab/app/src/main/java/com/example/cabdab/UploadActivity.java
@@ -4,18 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -24,15 +30,18 @@ import com.google.firebase.storage.UploadTask;
 import java.io.IOException;
 import java.util.UUID;
 
-public class UploadActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnChoose, btnUpload;
     private ImageView imageView;
+    private EditText editText;
+    private TextView textViewShow;
 
     private Uri filePath;
 
-    private final int PICK_IMAGE_REQUEST = 71;
+    private final int PICK_IMAGE_REQUEST = 234;
 
     private StorageReference mStorageRef;
+
 
 
     @Override
@@ -41,25 +50,18 @@ public class UploadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_upload);
         mStorageRef = FirebaseStorage.getInstance().getReference();
 
+        
+        btnChoose =(Button) findViewById(R.id.btnChoose);
+        btnUpload =(Button) findViewById(R.id.btnUpload);
+        imageView =(ImageView) findViewById(R.id.imgView);
+        editText = (EditText) findViewById(R.id.editText);
+        textViewShow = (TextView) findViewById(R.id.textViewShow);
 
-        btnChoose = findViewById(R.id.btnChoose);
-        btnUpload = findViewById(R.id.btnUpload);
-        imageView = findViewById(R.id.imgView);
-
-        btnChoose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chooseImage();
-            }
-        });
-
-        btnUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadImage();
-            }
-        });
+        btnChoose.setOnClickListener(this);
+        btnUpload.setOnClickListener(this);
+        textViewShow.setOnClickListener(this);
     }
+
     private void chooseImage() {
         Intent intent = new Intent();
         intent.setType("image/*");
@@ -83,6 +85,12 @@ public class UploadActivity extends AppCompatActivity {
             }
         }
     }
+
+    public String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
     private void uploadImage() {
 
         if(filePath != null)
@@ -91,7 +99,7 @@ public class UploadActivity extends AppCompatActivity {
             progressDialog.setTitle("Uploading...");
             progressDialog.show();
 
-            StorageReference ref = mStorageRef.child("images/"+ UUID.randomUUID().toString());
+            StorageReference ref = mStorageRef.child("images/"+ editText.getText().toString());
             ref.putFile(filePath)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
@@ -118,4 +126,16 @@ public class UploadActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnChoose) {
+            chooseImage();
+        } else if (v == btnUpload) {
+            uploadImage();
+
+        } else if (v == textViewShow) {
+
+        }
+    }
 }
